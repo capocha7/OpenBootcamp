0d012d59143229bf19343ea291143e685b510d09
capocha7 <federicopkrohn@gmail.com>
2023-03-27 16:54:03 -0300

    Ejercicio 7

---
+++ 7.1/Ejercicio 7.py
@@ -0,0 +1,17 @@
+from Ejercicio_7_modulo import *
+
+calculadora=Calculadora()
+
+print(calculadora.Suma(5,4))
+print(calculadora.Resta(5,4))
+print(calculadora.Division(5,4))
+print(calculadora.Multiplicacion(5,4))
+
+
+
+
+
+
+
+
+
---
+++ 7.1/Ejercicio_7_modulo.py
@@ -0,0 +1,15 @@
+class Calculadora():
+    def Suma(self,x,y):
+        return x+y
+    
+    def Resta(self,x,y):
+        return x-y
+    
+    def Multiplicacion(self,x,y):
+        return x*y
+    
+    def Division(self,x,y):
+        return x/y
+    
+
+    
